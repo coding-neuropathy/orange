ec4f07d214f29e7701e36062787d6f1ac8294a63
谢 家欣 <edison7500@gmail.com>
2015-05-25 10:56:13 +0800

    update

---
+++ orange WatchKit Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  orange WatchKit Extension
+//
+//  Created by 谢 家欣 on 15/5/25.
+//  Copyright (c) 2015年 guoku.com. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
