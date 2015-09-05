876b6a569ae4119d058c88c5d258b5e89e2c93d7
jiaxin <edison7500@gmail.com>
2015-09-05 17:28:02 +0800

    add articles controller

---
+++ orange/Class/SelectionViewController/ArticlesController.m
@@ -0,0 +1,56 @@
+//
+//  ArticlesController.m
+//  orange
+//
+//  Created by 谢家欣 on 15/9/5.
+//  Copyright (c) 2015年 guoku.com. All rights reserved.
+//
+
+#import "ArticlesController.h"
+
+@interface ArticlesController ()
+
+@property (strong, nonatomic) NSMutableArray * articleArray;
+@property (strong, nonatomic) UICollectionView * collectionView;
+
+@end
+
+@implementation ArticlesController
+
+#pragma mark - init View
+- (UICollectionView *)collectionView
+{
+    if (!_collectionView) {
+        UICollectionViewFlowLayout * layout = [[UICollectionViewFlowLayout alloc] init];
+        layout.scrollDirection = UICollectionViewScrollDirectionVertical;
+        //        layout.parallaxHeaderAlwaysOnTop = YES;
+        _collectionView = [[UICollectionView alloc] initWithFrame:CGRectMake(0., 0., kScreenWidth, kScreenHeight) collectionViewLayout:layout];
+
+        _collectionView.delegate = self;
+        _collectionView.dataSource = self;
+        _collectionView.backgroundColor = UIColorFromRGB(0xffffff);
+    }
+    return _collectionView;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
