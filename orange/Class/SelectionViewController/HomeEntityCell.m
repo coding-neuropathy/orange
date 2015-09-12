ae0f5a4d5488b9b430f0a7293789bb90bd4b53df
jiaxin <edison7500@gmail.com>
2015-09-12 21:19:40 +0800

    update

--- orange/Class/SelectionViewController/HomeEntityCell.m
+++ orange/Class/SelectionViewController/HomeEntityCell.m
@@ -109,7 +109,7 @@
     
     [self addObserver];
     
-    [self.imageView sd_setImageWithURL:_entity.imageURL_310x310];
+    [self.imageView sd_setImageWithURL:_entity.imageURL_310x310 placeholderImage:[UIImage imageWithColor:UIColorFromRGB(0xF0F0F0) andSize:CGSizeMake(self.imageView.deFrameWidth, self.imageView.deFrameHeight)]];
     self.titleLabel.text = _entity.title;
     self.detailLabel.text = _note.text;
     
