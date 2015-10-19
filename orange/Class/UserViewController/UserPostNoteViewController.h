001d3d493c32cf334ddb4d774ce50f25617897cc
jiaxin <edison7500@gmail.com>
2015-10-20 00:57:14 +0800

    update

---
+++ orange/Class/UserViewController/UserPostNoteViewController.h
@@ -0,0 +1,13 @@
+//
+//  UserPostNoteViewController.h
+//  orange
+//
+//  Created by 谢家欣 on 15/10/20.
+//  Copyright © 2015年 guoku.com. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface UserPostNoteViewController : BaseViewController
+
+@end
