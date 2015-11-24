74c718205fa185b5e27f8ba5e50e8ce24146ea51
jiaxin <edison7500@gmail.com>
2015-11-24 18:43:03 +0800

    update

--- orange/Class/PreViewControllers/EntityPreViewController.m
+++ orange/Class/PreViewControllers/EntityPreViewController.m
@@ -58,57 +58,7 @@
         [MobClick event:@"like_click" attributes:@{@"entity":self.entity.title} counter:(int)self.entity.likeCount];
         
         [API likeEntityWithEntityId:self.entity.entityId isLike:YES success:^(BOOL liked) {
-//            if (liked == self.likeButton.selected) {
-//                UIImageView * image = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"liked"]];
-//                image.frame = self.likeButton.imageView.frame;
-//                [self.likeButton addSubview:image];
-//                [UIView animateWithDuration:0.25 delay:0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
-//                    image.transform = CGAffineTransformScale(image.transform, 1.5, 1.5);
-//                    image.deFrameTop = image.deFrameTop - 10;
-//                    image.alpha = 0.1;
-//                }completion:^(BOOL finished) {
-//                    [image removeFromSuperview];
-//                }];
-//                ////[SVProgressHUD showImage:nil status:@"\U0001F603喜爱成功"];
-//            }
-//            
-//            
-//            self.likeButton.selected = liked;
             self.entity.liked = liked;
-//
-//            DDLogInfo(@"entity view %d", self.likeButton.selected);
-//            
-//            
-//            if (liked) {
-//                self.entity.likeCount += 1;
-//                UIImageView * image = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"liked"]];
-//                image.frame = self.likeButton.imageView.frame;
-//                [self.likeButton addSubview:image];
-//                
-//                [UIView animateWithDuration:0.25 delay:0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
-//                    image.transform = CGAffineTransformScale(image.transform, 1.5, 1.5);
-//                    image.deFrameTop = image.deFrameTop - 10;
-//                    image.alpha = 0.1;
-//                }completion:^(BOOL finished) {
-//                    [image removeFromSuperview];
-//                }];
-//                
-//                if ([Passport sharedInstance].user) {
-//                    [self.dataArrayForlikeUser insertObject:[Passport sharedInstance].user atIndex:0];
-//                }
-                ////[SVProgressHUD showImage:nil status:@"\U0001F603喜爱成功"];
-//            } else {
-//                [self.dataArrayForlikeUser removeObject:[Passport sharedInstance].user];
-//                
-//                self.entity.likeCount -= 1;
-//                [SVProgressHUD dismiss];
-//            }
-//            [self.collectionView reloadSections:[NSIndexSet indexSetWithIndex:4]];
-//            if (btn){
-//                btn.selected = self.entity.liked;
-//                [btn setTitle:[NSString stringWithFormat:@"%ld", self.entity.likeCount] forState:UIControlStateNormal];
-//            }
-//            [self setNavBarButton:self.flag];
         } failure:^(NSInteger stateCode) {
             [SVProgressHUD showImage:nil status:@"喜爱失败"];
         }];
