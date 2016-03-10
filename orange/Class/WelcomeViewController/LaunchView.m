ec56877809eedb5f13259e7ef7e8a5c1205d0db1
D_Collin <chendi@guoku.com>
2016-03-10 15:30:53 +0800

    启动画面修复

--- orange/Class/WelcomeViewController/LaunchView.m
+++ orange/Class/WelcomeViewController/LaunchView.m
@@ -25,8 +25,10 @@
     if (!_launchImage) {
         _launchImage = [[UIImageView alloc] initWithFrame:CGRectZero];
         _launchImage.contentMode = UIViewContentModeScaleAspectFill;
+        _launchImage.layer.cornerRadius = 4.;
         _launchImage.layer.masksToBounds = YES;
         _launchImage.userInteractionEnabled = YES;
+        
         UITapGestureRecognizer * tap = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(TapActionBtn:)];
         [self.launchImage addGestureRecognizer:tap];
 //        _launchImage.backgroundColor = UIColorFromRGB(0xf1f1f1);
@@ -36,31 +38,31 @@
     return _launchImage;
 }
 
-- (UILabel *)titleLabel
-{
-    if (!_titleLabel) {
-        _titleLabel = [[UILabel alloc] initWithFrame:CGRectZero];
-        _titleLabel.font = [UIFont systemFontOfSize:20.];
-        _titleLabel.textColor = UIColorFromRGB(0x414243);
-        _titleLabel.textAlignment = NSTextAlignmentCenter;
-        [self addSubview:_titleLabel];
-    }
-    return _titleLabel;
-}
+//- (UILabel *)titleLabel
+//{
+//    if (!_titleLabel) {
+//        _titleLabel = [[UILabel alloc] initWithFrame:CGRectZero];
+//        _titleLabel.font = [UIFont systemFontOfSize:20.];
+//        _titleLabel.textColor = UIColorFromRGB(0x414243);
+//        _titleLabel.textAlignment = NSTextAlignmentCenter;
+//        [self addSubview:_titleLabel];
+//    }
+//    return _titleLabel;
+//}
 
-- (UILabel *)detailLabel
-{
-    if (!_detailLabel) {
-        _detailLabel = [[UILabel alloc] initWithFrame:CGRectZero];
-        _detailLabel.font = [UIFont systemFontOfSize:14.];
-        _detailLabel.textColor = UIColorFromRGB(0x9d9e9f);
-        _detailLabel.textAlignment = NSTextAlignmentCenter;
-        _detailLabel.numberOfLines = 2;
-        
-        [self addSubview:_detailLabel];
-    }
-    return _detailLabel;
-}
+//- (UILabel *)detailLabel
+//{
+//    if (!_detailLabel) {
+//        _detailLabel = [[UILabel alloc] initWithFrame:CGRectZero];
+//        _detailLabel.font = [UIFont systemFontOfSize:14.];
+//        _detailLabel.textColor = UIColorFromRGB(0x9d9e9f);
+//        _detailLabel.textAlignment = NSTextAlignmentCenter;
+//        _detailLabel.numberOfLines = 2;
+//        
+//        [self addSubview:_detailLabel];
+//    }
+//    return _detailLabel;
+//}
 
 //- (UIButton *)actionBtn
 //{
@@ -80,16 +82,16 @@
     _launch = launch;
     
     [self.launchImage sd_setImageWithURL:_launch.launchImageURL_580];
-    self.titleLabel.text = _launch.title;
-    self.detailLabel.text = _launch.desc;
-    
-    NSMutableAttributedString *attributedString = [[NSMutableAttributedString alloc] initWithString:self.detailLabel.text];
-    NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
-    [paragraphStyle setLineSpacing:7.];
-    [attributedString addAttribute:NSParagraphStyleAttributeName value:paragraphStyle range:NSMakeRange(0, [self.detailLabel.text length])];
-    self.detailLabel.attributedText = attributedString;
-    
-    [self.actionBtn setTitle:[NSString stringWithFormat:@"%@ %@", _launch.action_title, [NSString fontAwesomeIconStringForEnum:FAArrowCircleORight]] forState:UIControlStateNormal];
+//    self.titleLabel.text = _launch.title;
+//    self.detailLabel.text = _launch.desc;
+//    
+//    NSMutableAttributedString *attributedString = [[NSMutableAttributedString alloc] initWithString:self.detailLabel.text];
+//    NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
+//    [paragraphStyle setLineSpacing:7.];
+//    [attributedString addAttribute:NSParagraphStyleAttributeName value:paragraphStyle range:NSMakeRange(0, [self.detailLabel.text length])];
+//    self.detailLabel.attributedText = attributedString;
+//    
+//    [self.actionBtn setTitle:[NSString stringWithFormat:@"%@ %@", _launch.action_title, [NSString fontAwesomeIconStringForEnum:FAArrowCircleORight]] forState:UIControlStateNormal];
     
     [self setNeedsLayout];
 }
@@ -100,11 +102,11 @@
     
 //    self.launchImage.frame = CGRectMake(0., 0., 290, 240.);
     
-    self.titleLabel.frame = CGRectMake(0., 0., self.deFrameWidth, 24.);
-    self.titleLabel.deFrameTop = 50;
-    
-    self.detailLabel.frame = CGRectMake(25., 0., self.deFrameWidth - 50., 60);
-    self.detailLabel.deFrameTop = self.titleLabel.deFrameBottom + 10.;
+//    self.titleLabel.frame = CGRectMake(0., 0., self.deFrameWidth, 24.);
+//    self.titleLabel.deFrameTop = 50;
+//    
+//    self.detailLabel.frame = CGRectMake(25., 0., self.deFrameWidth - 50., 60);
+//    self.detailLabel.deFrameTop = self.titleLabel.deFrameBottom + 10.;
     
 
     
