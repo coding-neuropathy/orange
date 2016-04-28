abb0cbaea7fd12d004bcc8311d6a4fc9e5706e31
D_Collin <chendi@guoku.com>
2016-04-28 15:00:31 +0800

    commit

--- core/API/API.m
+++ core/API/API.m
@@ -713,7 +713,6 @@
         
         if (success) {
             success(bannerArray, entityArray, categories, articles, users);
-            NSLog(@"%@",operation.response.URL);
         }
         
     } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
