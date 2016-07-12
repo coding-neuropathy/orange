56ed46e95a8a8fbfdbd8c0917a1cb747d8308287
jiaxin <edison7500@gmail.com>
2016-07-12 16:32:46 +0800

    add article comment model

---
+++ core/Model/Article/GKArticleComment.h
@@ -0,0 +1,35 @@
+//
+//  GKArticleComment.h
+//  orange
+//
+//  Created by 谢家欣 on 16/7/12.
+//  Copyright © 2016年 guoku.com. All rights reserved.
+//
+
+#import "GKBaseModel.h"
+#import "GKUser.h"
+
+@interface GKArticleComment : GKBaseModel
+
+/**
+ *  文章ID
+ */
+@property (assign, nonatomic) NSInteger articleId;
+
+/**
+ *  文章评论ID
+ */
+@property (assign, nonatomic) NSInteger commentId;
+
+/**
+ *  评论创建者
+ */
+@property (nonatomic, strong) GKUser *user;
+
+/**
+ *  评论内容
+ */
+@property (strong, nonatomic) NSString * content;
+
+
+@end
