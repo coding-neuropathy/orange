223ee79c4f220530d5571551f9f62f7c941d71af
jiaxin <edison7500@gmail.com>
2016-08-04 11:50:18 +0800

    update

---
+++ core/Model/Discover/GKDiscover.h
@@ -0,0 +1,13 @@
+//
+//  GKDiscover.h
+//  orange
+//
+//  Created by 谢家欣 on 16/8/4.
+//  Copyright © 2016年 guoku.com. All rights reserved.
+//
+
+#import "GKBaseModel.h"
+
+@interface GKDiscover : GKBaseModel
+
+@end
---
+++ core/Model/Discover/GKDiscover.m
@@ -0,0 +1,13 @@
+//
+//  GKDiscover.m
+//  orange
+//
+//  Created by 谢家欣 on 16/8/4.
+//  Copyright © 2016年 guoku.com. All rights reserved.
+//
+
+#import "GKDiscover.h"
+
+@implementation GKDiscover
+
+@end
