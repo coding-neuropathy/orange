096abd150449387418dcac15d3bc5659ddb3ee8b
jiaxin <edison7500@gmail.com>
2016-08-05 11:29:32 +0800

    update

--- core/Model/GKUser.m
+++ core/Model/GKUser.m
@@ -40,6 +40,7 @@
                              @"location"         : @"location",
                              @"mail_verified'"   : @"mail_verified",
                              @"authorized_author": @"authorized_author",
+                             @"authorized_seller": @"authorized_seller",
                              @"nick"             : @"nick",
                              };
     
