5aafc669514d1b9eb3e31f0c56cf376f9e6c501a
jiaxin <edison7500@gmail.com>
2016-09-18 16:23:04 +0800

    update

--- orange/AppDelegate.m
+++ orange/AppDelegate.m
@@ -338,7 +338,7 @@ int ddLogLevel;
     {
         return [WXApi handleOpenURL:url delegate:[ThreePartHandler sharedThreePartHandler]];
     }
-    return [WeiboSDK handleOpenURL:url delegate:self];
+    return [WeiboSDK handleOpenURL:url delegate:[ThreePartHandler sharedThreePartHandler]];
 }
 
 - (void)application:(UIApplication *)app didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
@@ -369,73 +369,6 @@ int ddLogLevel;
 {
     [JPUSHService handleRemoteNotification:userInfo];
     completionHandler(UIBackgroundFetchResultNewData);
-}
-
-#pragma mark - weibo delegate
-- (void)didReceiveWeiboRequest:(WBBaseRequest *)request
-{
-    
-}
-
-- (void)didReceiveWeiboResponse:(WBBaseResponse *)response
-{
-    if ([response isKindOfClass:WBAuthorizeResponse.class])
-    {
-//        WBSendMessageToWeiboResponse* sendMessageToWeiboResponse = (WBSendMessageToWeiboResponse*)response;
-        NSString* accessToken = [(WBAuthorizeResponse *)response accessToken];
-        [[NSUserDefaults standardUserDefaults] setObject:accessToken forKey:@"wbtoken"];
-        [[NSUserDefaults standardUserDefaults] synchronize];
-        [[NSNotificationCenter defaultCenter] postNotificationName:@"WBAuthResp" object:response.userInfo];
-    }
-
-}
-
-- (void)onResp:(BaseResp *)resp
-{
-//    DDLogInfo(@"resp %@", resp);
-//    NSInteger wechatType = [[[NSUserDefaults standardUserDefaults] valueForKeyPath:kWechatType] integerValue];
-    if ([resp isKindOfClass:[SendMessageToWXResp class]]) {
-//        DDLogError(@"error code %d", resp.errCode);
-        switch (resp.errCode) {
-            case 0:
-            {
-                double delayInSeconds = 0.5;
-                dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
-                dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
-//                    [SVProgressHUD showImage:nil status:@"分享成功"];
-                    [SVProgressHUD showSuccessWithStatus:NSLocalizedStringFromTable(@"share-success", kLocalizedFile, nil)];
-                });
-            }
-                break;
-            case -2:
-                
-                break;
-            default:
-            {
-                double delayInSeconds = 0.5;
-                dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
-                dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
-//                    [SVProgressHUD showImage:nil status:@"分享失败"];
-                    [SVProgressHUD showErrorWithStatus:NSLocalizedStringFromTable(@"share-failure", kLocalizedFile, nil)];
-                });
-            }
-                break;
-        }
-    }
-    
-    if([resp isKindOfClass:[SendAuthResp class]]) {
-//        DDLogInfo(@"resp %@", [(SendAuthResp *)resp code]);
-        switch (resp.errCode) {
-            case 0:
-                [[NSNotificationCenter defaultCenter] postNotificationName:@"WechatAuthResp" object:resp];
-                break;
-                
-            default:
-                break;
-        }
-        
-    }
-
 }
 
 
